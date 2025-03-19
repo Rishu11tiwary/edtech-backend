c290dd1cf9d60d1e46b93a748a4617c553553a1d
Rishu Tiwary <rishutiwary1108@gmail.com>
2025-03-19 12:36:53 +0530

    image fix

--- mail/emailVerificationTemplate.js
+++ mail/emailVerificationTemplate.js
@@ -66,8 +66,6 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
@@ -80,7 +78,7 @@ const otpTemplate = (otp) => {
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
 					href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
 		</div>
-	</body>
+</body>
 	
 	</html>`;
 };
